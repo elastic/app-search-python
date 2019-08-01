7fa49ef0b09f20540fab061260b1a8f9646c5e55
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-08-01 08:41:34 +0200

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
