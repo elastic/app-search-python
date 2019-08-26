0b621f9b17b5de9b108e3548fe487ed2fd792490
Jason Stoltzfus <jastoltz24@gmail.com>
2019-08-26 15:40:42 -0400

    Release v7.3.0 (#15)

--- elastic_app_search/__version__.py
+++ elastic_app_search/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'elastic-app-search'
 __description__ = 'An API client for Elastic App Search'
 __url__ = 'https://github.com/elastic/app-search-python'
-__version__ = '7.2.0'
+__version__ = '7.3.0'
 __author__ = 'Elastic'
 __author_email__ = 'support@elastic.co'
