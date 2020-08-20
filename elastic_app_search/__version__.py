b1366fdddb455841fcafaa42ac8ac20a35e8663a
Rich Kuzsma <62522248+richkuz@users.noreply.github.com>
2020-08-20 09:27:13 -0400

    Release v7.9.0

--- elastic_app_search/__version__.py
+++ elastic_app_search/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'elastic-app-search'
 __description__ = 'An API client for Elastic App Search'
 __url__ = 'https://github.com/elastic/app-search-python'
-__version__ = '7.8.1'
+__version__ = '7.9.0'
 __author__ = 'Elastic'
 __author_email__ = 'support@elastic.co'
